d237dfa4fb313399610c70bce688aea2f71070d4
Navjot Singh Rakhra <45345158+NavjotSRakhra@users.noreply.github.com>
2023-09-14 16:50:54 +0530

    Initial commit

---
+++ src/test/java/io/github/navjotsrakhra/internalsecureproxy/InternalSecureProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.navjotsrakhra.internalsecureproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalSecureProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
